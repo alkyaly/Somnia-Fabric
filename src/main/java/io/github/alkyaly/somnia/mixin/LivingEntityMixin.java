42599c1187f00fc3df731754bf3b6939bed3074e
adrianoy <adriano.yt624@gmail.com>
2021-10-01 03:53:42 -0300

    It wasn't a parsing error... LivingEntity#finishUsingItem shrinks the stack, if the size was 1, it would set it to air, not matching our resource location comparison with entries

--- src/main/java/io/github/alkyaly/somnia/mixin/LivingEntityMixin.java
+++ src/main/java/io/github/alkyaly/somnia/mixin/LivingEntityMixin.java
@@ -6,6 +6,7 @@ import io.github.alkyaly.somnia.core.Somnia;
 import net.minecraft.core.Registry;
 import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.UseAnim;
 import org.spongepowered.asm.mixin.Mixin;
@@ -22,27 +23,27 @@ public abstract class LivingEntityMixin {
     //No similar event in fabric api
     @Inject(
             at = @At(
-                    value = "INVOKE_ASSIGN",
+                    value = "INVOKE",
                     target = "Lnet/minecraft/world/item/ItemStack;finishUsingItem(Lnet/minecraft/world/level/Level;Lnet/minecraft/world/entity/LivingEntity;)Lnet/minecraft/world/item/ItemStack;"
             ),
             method = "completeUsingItem"
     ) // Forge: LivingEntityUseItemEvent.Finish on ForgeEventHandler
     private void somnia$onFinishUsing(CallbackInfo ci) {
-        ItemStack stack = useItem;
+        Item item = useItem.getItem();
         //We can't check against food properties, most "drinks" don't add one.
-        boolean eat = stack.getUseAnimation() == UseAnim.DRINK || stack.getUseAnimation() == UseAnim.EAT;
+        boolean eat = useItem.getUseAnimation() == UseAnim.DRINK || useItem.getUseAnimation() == UseAnim.EAT;
 
         //noinspection ConstantConditions, InstanceofThis
         if (eat && (Object) this instanceof Player player) {
             for (ReplenishingItemEntry entry : Somnia.CONFIG.fatigue.replenishingItems) {
-                if (Registry.ITEM.getKey(stack.getItem()).toString().equals(entry.item())) {
+                if (Registry.ITEM.getKey(item).toString().equals(entry.item())) {
                     SomniaAPI.modifyAttributesFromEntry(player, entry);
                     return;
                 }
             }
 
             for (ReplenishingItemEntry entry : SomniaAPI.getReplenishingItems()) {
-                if (Registry.ITEM.getKey(stack.getItem()).toString().equals(entry.item())) {
+                if (Registry.ITEM.getKey(item).toString().equals(entry.item())) {
                     SomniaAPI.modifyAttributesFromEntry(player, entry);
                     return;
                 }
